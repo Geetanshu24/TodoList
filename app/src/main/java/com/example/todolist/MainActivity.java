6d7932a47cd64d5eef72c4f2d605dde3a63d3e0c
Geetanshu24 <geetanshubansal2001@gmail.com>
2023-03-28 12:01:57 -0700

    Commit changes

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.todolist.Adapter.TodoAdapter;
 import com.example.todolist.Model.TodoModel;
 import com.example.todolist.Utils.DataBaseHandler;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Collections;
@@ -24,6 +32,12 @@ public class MainActivity extends AppCompatActivity implements  DialogCloseListn
     private RecyclerView tasksRecyclerView;
     private TodoAdapter tasksAdapter;
     private FloatingActionButton fab;
+    private FloatingActionButton logout;
+
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
 
     private List<TodoModel> taskList;
 
@@ -45,16 +59,53 @@ public class MainActivity extends AppCompatActivity implements  DialogCloseListn
         itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
 
         fab = findViewById(R.id.fab);
+        logout = findViewById(R.id.logout);
 
         taskList = db.getAllTasks();
         Collections.reverse(taskList);
 
         tasksAdapter.setTasks(taskList);
 
+
+
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null){
+
+
+
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SignInOut();
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+    }
+
+    private void SignInOut() {
+
+
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),Google_sign_inActivity.class));
             }
         });
     }
@@ -65,5 +116,6 @@ public class MainActivity extends AppCompatActivity implements  DialogCloseListn
         Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
         tasksAdapter.notifyDataSetChanged();
+
     }
 }
